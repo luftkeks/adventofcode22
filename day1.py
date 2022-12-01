13fbc3784e550df1baf6f668a31ca9a5669d5fa2
Luft <luft171090@gmail.com>
2022-12-01 08:59:00 +0100

    warum auch immer das nicht durchn proxy kommt
    egal dann mach ichs halt von hand

---
+++ day1.py
@@ -0,0 +1,15 @@
+file = open("input.txt","r")
+lines = file.readlines()
+number = 0
+numbers = []
+for line in lines:
+    if line != "\n":
+        number += int(line)
+    else:
+        numbers.append(number)
+        number = 0
+
+print(max(numbers))
+numbers = sorted(numbers,reverse=True)
+print(numbers)
+print(numbers[0]+numbers[1]+numbers[2])
