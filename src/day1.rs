3f025bc206845a982dddd8922c0898ba68057b01
luftkeks <keksluft@gmail.com>
2022-12-03 23:52:40 +0100

    cargo fmt in zukunft dann automatisch

--- src/day1.rs
+++ src/day1.rs
@@ -1,10 +1,9 @@
-
-pub fn day1(){
+pub fn day1() {
     let input = std::fs::read_to_string("input1.txt").unwrap();
     let mut number = 0;
     let mut numbers = vec![];
-    for line in input.lines(){
-        if !line.is_empty(){
+    for line in input.lines() {
+        if !line.is_empty() {
             number += line.trim().parse::<i32>().unwrap();
         } else {
             numbers.push(number);
@@ -15,5 +14,5 @@ pub fn day1(){
     numbers.sort();
     numbers.reverse();
     println!("Solution Day1a: {}", numbers.first().unwrap());
-    println!("Solution Day1b: {}", numbers[0]+numbers[1]+numbers[2])
+    println!("Solution Day1b: {}", numbers[0] + numbers[1] + numbers[2])
 }
