637f1ba3f6d6b98f582b0946b7c1957b46d68f72
luftkeks <keksluft@gmail.com>
2022-12-03 23:40:58 +0100

    Rust day 1

---
+++ src/day1.rs
@@ -0,0 +1,19 @@
+
+pub fn day1(){
+    let input = std::fs::read_to_string("input1.txt").unwrap();
+    let mut number = 0;
+    let mut numbers = vec![];
+    for line in input.lines(){
+        if !line.is_empty(){
+            number += line.trim().parse::<i32>().unwrap();
+        } else {
+            numbers.push(number);
+            number = 0
+        }
+    }
+
+    numbers.sort();
+    numbers.reverse();
+    println!("Solution Day1a: {}", numbers.first().unwrap());
+    println!("Solution Day1b: {}", numbers[0]+numbers[1]+numbers[2])
+}
